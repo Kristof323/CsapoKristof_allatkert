7ed6e5ed6dab6e99eddaef7e72b941573404af34
Csapó Kristóf Zoltán <kristofcsapo@gmail.com>
2025-02-28 11:09:26 +0100

    megcsináltam a feladatot

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace ConsoleApp7
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Random rand = new Random();
+        List<allatok> animals = new List<allatok>
         {
-            Console.WriteLine("Hello, World!");
+            new viziallat("Delfin", rand.Next(10, 30)),
+            new hidegallat("Jegesmedve", rand.Next(5, 15)),
+            new mediterranallat("Muflon", rand.Next(10, 20)),
+            new melegallat("Oroszlán", rand.Next(15, 25)),
+            new tropusiallat("Jaguár", rand.Next(20, 30))
+        };
+
+        int zooCapacity = rand.Next(5, 15);
+        List<allatok> zoo = new List<allatok>(animals.GetRange(0, Math.Min(zooCapacity, animals.Count)));
+
+        Console.WriteLine($"\nÁllatkert kapacitása: {zooCapacity}, Jelenlegi állatok száma: {zoo.Count}");
+        int filledPercentage = (int)((double)zoo.Count / zooCapacity * 10);
+        Console.WriteLine("Állatkert telítettsége: [" + new string('#', filledPercentage) + new string('-', 10 - filledPercentage) + "]");
+
+        Race(zoo);
+    }
+
+    static void Race(List<allatok> allatok)
+    {
+        Console.WriteLine("\nVerseny kezdődik! Ki a leggyorsabb?");
+        int maxSpeed = 0;
+        foreach (var allat in allatok)
+        {
+            if (allat.Speed > maxSpeed)
+                maxSpeed = allat.Speed;
+        }
+
+        for (int step = 0; step < maxSpeed; step++)
+        {
+            foreach (var allat in allatok)
+            {
+                if (step < allat.Speed)
+                {
+                    Console.WriteLine($"{allat.Name,-10} |" + new string(' ', step) + "O");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ ConsoleApp7/allatok.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+abstract class allatok
+{
+    public string Name { get; set; }
+    public int Speed { get; set; }
+
+    public allatok(string name, int speed)
+    {
+        Name = name;
+        Speed = speed;
+    }
+}
+
+class viziallat : allatok
+{
+    public viziallat(string name, int speed) : base(name, speed) { }
+}
+
+class hidegallat : allatok
+{
+    public hidegallat(string name, int speed) : base(name, speed) { }
+}
+
+class mediterranallat : allatok
+{
+    public mediterranallat(string name, int speed) : base(name, speed) { }
+}
+
+class melegallat : allatok
+{
+    public melegallat(string name, int speed) : base(name, speed) { }
+}
+
+class tropusiallat : allatok
+{
+    public tropusiallat(string name, int speed) : base(name, speed) { }
+}
